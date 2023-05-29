88e649e2c8bc35aa3dd20ea23cb396e78b1a3471
Sampo Hakola <sampo.hakola@helsinki.fi>
2023-05-29 20:16:10 +0300

    4.1 ja 4.2 ok

--- part4/blogi_backend/index.js
+++ part4/blogi_backend/index.js
@@ -1,27 +1,6 @@
+const app = require('./app')
 const config = require('./utils/config')
-const http = require('http')
-const express = require('express')
-const app = express()
-const cors = require('cors')
-const blogRouter = require('./controllers/blogs')
-const mongoose = require('mongoose')
 
-/*
-const blogSchema = mongoose.Schema({
-    title: String,
-    author: String,
-    url: String,
-    likes: Number
-})
-
-const Blog = mongoose.model('Blog', blogSchema)
-*/
-const mongoUrl = config.MONGODB_URI
-mongoose.connect(mongoUrl)
-
-app.use(cors())
-app.use(express.json())
-app.use('/api/blogs', blogRouter)
 
 app.listen(config.PORT, () => {
     console.log(`Server running on port ${config.PORT}`)
