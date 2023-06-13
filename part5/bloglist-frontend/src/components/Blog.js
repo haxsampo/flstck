f33f3c9496feaa3bcea970e793152dcddae9f61a
Sampo Hakola <sampo.hakola@helsinki.fi>
2023-06-13 22:56:27 +0300

    5.7 ok

--- part5/bloglist-frontend/src/components/Blog.js
+++ part5/bloglist-frontend/src/components/Blog.js
@@ -1,7 +1,48 @@
-const Blog = ({blog}) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+import { useState } from "react"
+
+const Blog = ({ blog }) => {
+  const [showFull, setShowFull] = useState(false)
+
+
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5
+  }
+  const hideWhenVisible = { display: showFull ? 'none' : '' }
+  const showWhenVisible = { display: showFull ? '' : 'none' }
+
+  const fullBlog = (blog) => (
+    <div>
+      {blog.title} {blog.author} {blog.likes} {blog.url}
+    </div>
+  )
+
+  const miniBlog = (blog) => (
+    <div>
+      {blog.title}
+    </div>
+  )
+
+  const toggleVisibility = () => {
+    setShowFull(!showFull)
+  }
+
+  return (
+    <div style={blogStyle}>
+      <div style={hideWhenVisible}>
+        <button onClick={toggleVisibility}>show</button>
+      </div>
+      <div style={showWhenVisible}>
+        <button onClick={toggleVisibility}>hide</button>
+      </div>
+      {showFull && fullBlog(blog)}
+      {!showFull && miniBlog(blog)}
+
+    </div>
+  )
+}
 
 export default Blog
